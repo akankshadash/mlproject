1db9606d815912dfb92c1b24c86aadc341db1ac1
akankshadash <akankshadash@Akankshas-MacBook-Pro.local>
2025-06-12 19:14:26 +0530

    logging and exception

---
+++ source/loggers.py
@@ -0,0 +1,21 @@
+import sys
+import logging
+import os
+from datetime import datetime
+from exceptions import CustomException
+
+Log_file = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+log_path = os.path.join(os.getcwd(), "logs", Log_file)
+os.makedirs(os.path.dirname(log_path), exist_ok=True)
+
+Log_file_path = os.path.join(os.getcwd(), "logs", Log_file)
+
+logging.basicConfig(
+    filename=Log_file_path,
+    format='[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+
+    
